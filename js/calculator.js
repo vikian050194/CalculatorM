7dcf633a7836ed7b32b78baa0fae25608eed9773
Vinogradov Kirill <vikian050194@gmail.com>
2016-06-10 16:22:12 +0300

    add engine.js

--- js/calculator.js
+++ js/calculator.js
@@ -1,6 +1,7 @@
 function Calculator(c) {
     var calc = $(c);
     var output = calc.find('#output')
+    var module = calc.find('#module')
     var engine = new Engine();
 
     var get = function () {
@@ -17,23 +18,47 @@ function Calculator(c) {
             newValue += i;
         }
         set(newValue);
+		engine.append(parseInt(i));
     }
     var clean = function () {
         set('0');
-        engine.currentValue = 0;
+        engine.clean();
     }
+	var setMod = function(){
+		var value = get();
+		module.html(value);
+		engine.setMod();
+		clean();
+	}
+	var clickDigit = function(e) {
+		var value = e.currentTarget.firstChild.data;
+		append(value)
+    }
+	var clickOperator = function(e){
+		var value = e.currentTarget.firstChild.data;
+		engine.setOperator(value)
+		set('0');
+	}
+	var calculate = function()
+	{
+		var result = engine.calculate();
+		set(result);
+	}
     var init = function () {
-        $('#c').on('click', clean);
-
+		calc.find('#setMod').on('click', setMod);
+		calc.find('#calculate').on('click', calculate);
+        calc.find('#c').on('click', clean);
         for (var i = 0; i <= 9; i++) {
             var id = '#' + i;
-            $(id).on('click', function (e) {
-                var value = e.currentTarget.firstChild.data;
-                append(value)
-            });
+            calc.find(id).on('click', clickDigit);
         }
-    }
-
+		var operators = ['add','mul','sub','div','mod','pow'];
+		for(var i = 0;i<=operators.length;i++){
+			var id = '#' + operators[i];
+            calc.find(id).on('click', clickOperator);
+		}		
+    }	
+	
     init();
 }
 
--- js/engine.js
+++ js/engine.js
@@ -1,3 +1,96 @@
 function Engine() {
-    this.currentValue = 0;
+    var firstArgument = 0;
+    var secondArgument = 0;
+    var module = 1;
+	var state = 0;
+	var operator = '';
+	
+	this.clean = function () {
+		firstArgument = 0;
+		secondArgument = 0;
+		state = 0;
+		operator = '';
+	}
+	this.setMod = function () {
+		module = firstArgument;
+		firstArgument = 0;
+	}
+	this.calculate = function () {
+		function mod(){
+			return firstArgument % secondArgument;
+		}
+		function add(){
+			return (firstArgument + secondArgument) % module;
+		}
+		function sub(){
+			return (firstArgument - secondArgument) % module;
+		}
+		function mul(){
+			return (firstArgument * secondArgument) % module;
+		}
+		function div(){
+			return (firstArgument / secondArgument) % module;
+		}
+		function pow(){
+			var result = 1;
+			
+			for(var i = 1; i <= secondArgument; i++){
+				result = result * firstArgument % module;
+			}
+			
+			return result;
+		}
+		
+		var result = 0;
+		
+		switch(operator) {
+		case '':
+		result = firstArgument % module;
+		break;
+		case 'mod':
+		result = mod();
+		break;
+		case 'add':
+		result = add();
+		break;
+		case 'sub':
+		result = sub();
+		break;
+		case 'mul':
+		result = mul();
+		break;
+		case 'div':
+		result = div();
+		break;
+		case 'pow':
+		result = pow();
+		break;
+		default:
+		
+		break;
+		}
+		
+		state = 0;
+		operator = '';
+		firstArgument = result;
+		secondArgument = 0;
+		return result;
+	}
+	this.append = function(digit){
+		switch(state) {
+		case 0:
+		firstArgument = firstArgument * 10 + digit;
+		break;
+		case 1:
+		secondArgument = secondArgument * 10 + digit;
+		break;
+		default:
+		
+		break;
+		}
+	}
+	this.setOperator = function(param){
+		operator = param;
+		state = 1;
+	}
 }
