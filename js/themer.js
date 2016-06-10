3a33cf433728b54b8fd4e3ab9015666a8c999efa
Vinogradov Kirill <vikian050194@gmail.com>
2016-06-10 18:07:31 +0300

    add coolies feature

--- js/themer.js
+++ js/themer.js
@@ -1,21 +1,34 @@
 function Themer() {
-    var themes = ['blue', 'gray'];
-    var index = 0;
+    var themes = ['blue', 'gray', 'multi', 'pastel'];
     var loader = new Loader();
-    var nextIndex = function () {
-        index++;
-        index %= themes.length;
+
+    function nextIndex() {
+        currentThemeIndex++;
+        currentThemeIndex %= themes.length;
+        $.cookie("theme", currentThemeIndex);
     }
-    var getHref = function () {
-        var result = 'css/themes/' + themes[index] + '.css';
+    function getHref() {
+        var result = 'css/themes/' + themes[currentThemeIndex] + '.css';
         return result;
     }
-    this.next = function () {
-        nextIndex();
+    function loadCurrentTheme() {
         var href = getHref();
         var filetype = 'css';
         var id = 'theme';
         $('#' + id).remove();
         loader.load(href, filetype, id);
+    }
+
+    this.next = function () {
+        nextIndex();
+        loadCurrentTheme();
+    }
+
+    var currentThemeIndex = $.cookie("theme");
+    if (currentThemeIndex != undefined) {
+        loadCurrentTheme();
+    } else {
+        currentThemeIndex = 0;
+        $.cookie("theme", currentThemeIndex);
     }
 }
