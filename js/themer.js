7689a222fbc74967b61667d5c84adf459f53b549
Vinogradov Kirill <vikian050194@gmail.com>
2016-06-11 13:20:37 +0300

    refactoring

--- js/themer.js
+++ js/themer.js
@@ -1,8 +1,60 @@
+Date.prototype.addDays = function(days)
+{
+    var result = new Date(this.valueOf());
+    result.setDate(result.getDate() + days);
+    return result;
+}
+
+function Cookier() {
+    this.getCookie = function (name) {
+        var matches = document.cookie.match(new RegExp(
+            "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+        ));
+        return matches ? decodeURIComponent(matches[1]) : undefined;
+    }
+
+    this.setCookie = function (name, value, options) {
+        options = options || {};
+
+        var expires = options.expires;
+
+        if (typeof expires == "number" && expires) {
+            var d = new Date();
+            d.setTime(d.getTime() + expires * 1000);
+            expires = options.expires = d;
+        }
+        if (expires && expires.toUTCString) {
+            options.expires = expires.toUTCString();
+        }
+
+        value = encodeURIComponent(value);
+
+        var updatedCookie = name + "=" + value;
+
+        for (var propName in options) {
+            updatedCookie += "; " + propName;
+            var propValue = options[propName];
+            if (propValue !== true) {
+                updatedCookie += "=" + propValue;
+            }
+        }
+
+        document.cookie = updatedCookie;
+    }
+
+    this.deleteCookie = function (name) {
+        setCookie(name, "", {
+            expires: -1
+        })
+    }
+}
+
 function Themer() {
-    var themes = ['blue', 'gray', 'multi', 'pastel', 'russia', 'light'];
+    var themes = ['blue', 'gray', 'multi', 'pastel', 'russia-dark', 'russia-light', 'light'];
     var loader = new Loader();
+    var cookier = new Cookier();
     function saveInCookies() {
-        $.cookie("theme", currentThemeIndex, { expires: 31 });
+        cookier.setCookie("theme", currentThemeIndex, { expires: new Date().addDays(31) });
     }
     function nextIndex() {
         currentThemeIndex++;
@@ -26,7 +78,7 @@ function Themer() {
         loadCurrentTheme();
     }
 
-    var currentThemeIndex = $.cookie("theme");
+    var currentThemeIndex = cookier.getCookie("theme");
     if (currentThemeIndex == undefined) {
         currentThemeIndex = 0;
         saveInCookies();
