27b224a7bd6e6d51edf9dd127424d4be6ff6eba5
Furmanets Vitaly <furmanets_vit@mail.ru>
2017-09-19 00:47:55 +0300

    Refactoring integer; Add method getAdd; Write some tests for integer;

--- js/integer/integer.js
+++ js/integer/integer.js
@@ -1,28 +1,82 @@
-function Int(number) {
+function Integer(number) {
     var pattern = /-*\d/;
-    digits = [];
+    this.digits = [];
     this.sign = '+';
-    if (typeof number === "string" & pattern.test(number)) {
-        if (number[0] === '-') {
-            this.sign = '-';
+    if (number !== undefined) {
+        if (typeof number === "string" & pattern.test(number)) {
+            if (number[0] === '-') {
+                this.sign = '-';
+                for (var i = 1; i < number.length; i++) {
+                    this.digits[number.length - i - 1] = parseInt(number[i]);
+                }
+            }
+            else {
+                for (var i = 0; i < number.length; i++) {
+                    this.digits[number.length - i - 1] = parseInt(number[i]);
+                }
+            }
+
         }
-        for (var i = 1; i < number.length; i++) {
-            digits[number.length - i - 1] = parseInt(number[i]);
+        else {
+            throw "Format error!";
         }
     }
     else {
-        throw "Format error!";
+        this.digits[0] = 0;
     }
 
-    this.toString = function () {
-        var result ='';
-        if (this.sign === '-'){
-            result +='-';
+    this.getAdd = function (termFirst, termSecond) {
+        if ((typeof termFirst !== 'object') || (typeof termSecond !== 'object')) {
+            throw 'Error format in operation Add!'
         }
-            result += String(digits.reduce(function (previousValue, currentValue) {
-                return pacurrentValue + previousValue;
-            }));
+        else {
+            if (termFirst.sign === termSecond.sign) {
+                this.sign = termFirst.sign;
+                var i = 0;
+                var balance = 0;
+                while (i <= termFirst.digits.length - 1 && i <= termSecond.digits.length - 1) {
+                    this.digits[i] = termFirst.digits[i] + termSecond.digits[i] + balance;
+                    balance = 0;
+                    if (this.digits[i] >= 10) {
+                        balance = parseInt(this.digits[i] / 10);
+                        this.digits[i] %= 10;
+                    }
+                    i++;
+                }
+                if (termFirst.digits.length - 1 === termSecond.digits.length - 1) {
+                    this.digits[i] = balance;
+                }
+                if (termFirst.digits.length - 1 < termSecond.digits.length - 1) {
+                    while (i <= termSecond.digits.length - 1) {
+                        this.digits[i] = termSecond.digits[i] + balance;
+                        balance = 0;
+                        i++;
+                    }
+                }
+                if (termFirst.digits.length - 1 > termSecond.digits.length - 1) {
+                    while (i <= termFirst.digits.length - 1) {
+                        this.digits[i] = termFirst.digits[i] + balance;
+                        balance = 0;
+                        i++;
+                    }
+                }
+            }
+        }
 
-        return result;
+        }
+    
+
+
+this.toString = function () {
+    var result = '';
+    if (this.sign === '-') {
+        result += '-';
     }
-} 
+    result += String(this.digits.reduce(function (previousValue, currentValue) {
+        var str = '' + currentValue + previousValue;
+        return str;
+    }));
+
+    return result;
+}
+}
--- js/tests/test-integer.js
+++ js/tests/test-integer.js
@@ -6,7 +6,7 @@ function IntTestSet() {
     addNumber.author = 'Vitaly';
 
     addNumber.test = function () {
-        var number = new Int('2');
+        var number = new Integer('2');
         return number.toString();
     };
 
@@ -20,7 +20,7 @@ function IntTestSet() {
     addNegativeNumber.author = 'Vitaly';
 
     addNegativeNumber.test = function () {
-        var number = new Int('-2');
+        var number = new Integer('-2');
         return number.toString();
     };
 
@@ -34,14 +34,65 @@ function IntTestSet() {
     addBigNumber.author = 'Vitaly';
 
     addBigNumber.test = function () {
-        var number = new Int('-210');
+        var number = new Integer('-210');
         return number.toString();
     };
 
     addBigNumber.expectedObject = (function () {
-        return '-21';
+        return '-210';
     })();
     testSet.addTestItem(addBigNumber);
+
+    var getAmount = new TestItem();
+    getAmount.name = 'Get Amount';
+    getAmount.author = 'Vitaly';
+
+    getAmount.test = function () {
+        var number = new Integer('210');
+        var numberSecond = new Integer('95');
+        var result = new Integer();
+        result.getAdd(number, numberSecond);
+        return result.toString();
+    };
+
+    getAmount.expectedObject = (function () {
+        return '305';
+    })();
+    testSet.addTestItem(getAmount);
+
+    var getAmountNegativeNumbers = new TestItem();
+    getAmountNegativeNumbers.name = 'Get Amount';
+    getAmountNegativeNumbers.author = 'Vitaly';
+
+    getAmountNegativeNumbers.test = function () {
+        var numberFirst = new Integer('-87');
+        var numberSecond = new Integer('-59');
+        var result = new Integer();
+        result.getAdd(numberFirst, numberSecond);
+        return result.toString();
+    };
+
+    getAmountNegativeNumbers.expectedObject = (function () {
+        return '-146';
+    })();
+    testSet.addTestItem(getAmountNegativeNumbers);
+
+    var getAmountBigNumbers = new TestItem();
+    getAmountBigNumbers.name = 'Get Amount';
+    getAmountBigNumbers.author = 'Vitaly';
+
+    getAmountBigNumbers.test = function () {
+        var numberFirst = new Integer('21037831813');
+        var numberSecond = new Integer('959');
+        var result = new Integer();
+        result.getAdd(numberFirst, numberSecond);
+        return result.toString();
+    };
+
+    getAmountBigNumbers.expectedObject = (function () {
+        return '21037832772';
+    })();
+    testSet.addTestItem(getAmountBigNumbers);
 
     return testSet.test();
 };
