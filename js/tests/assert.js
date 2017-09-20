36896ee911001674b7354777d1f2631cce9d3f62
Vinogradov Kirill <vikian050194@gmail.com>
2017-09-20 09:58:40 +0300

    Fix problem with comparision of arrays;

--- js/tests/assert.js
+++ js/tests/assert.js
@@ -18,38 +18,37 @@ function compare(expected, actual) {
     if (typeof (actual) !== typeof (expected)) {
         throwError('type', typeof (expected), typeof (actual));
     }
-	
-	if (actual === null && expected !== null) {
+
+    if (actual === null && expected !== null) {
         return throwError('actual', expected[p], actual[p]);
     }
-				
-	if (actual === null && expected === null) {
+
+    if (actual === null && expected === null) {
         return;
     }
 
     switch (typeof (actual)) {
-        case 'array':
-            if (actual.length !== expected.length) {
-                throwError('array length', expected.length, actual.length);
-            }
-
-            actual.forEach(function (element, index) {
-                if (element !== expected[index]) {
-                    throwError('array element', expected[index], element);
-                }
-            }, this);
-
         case 'object':
-            if (Object.keys(actual).length !== Object.keys(expected).length) {
-                throwError('objects keys count', Object.keys(expected).length, Object.keys(actual).length);
-            }
-
-            for (var p in actual) {
-                if (actual.hasOwnProperty(p) !== expected.hasOwnProperty(p)) {
-                    throwError('object key', p, undefined);
+            if (Array.isArray(actual)) {
+                if (actual.length !== expected.length) {
+                    throwError('array length', expected.length, actual.length);
                 }
-				
-				compare(expected[p], actual[p]);	
+                
+                actual.forEach(function (element, index) {
+                    compare(expected[index], element);
+                }, this);
+            } else {
+                if (Object.keys(actual).length !== Object.keys(expected).length) {
+                    throwError('objects keys count', Object.keys(expected).length, Object.keys(actual).length);
+                }
+
+                for (var p in actual) {
+                    if (actual.hasOwnProperty(p) !== expected.hasOwnProperty(p)) {
+                        throwError('object key', p, undefined);
+                    }
+
+                    compare(expected[p], actual[p]);
+                }
             }
             break;
         case 'function':
