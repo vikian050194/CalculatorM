8eddbdae23efaf283fbe20191ddbd6037761dff4
Furmanets Vitaly <furmanets_vit@mail.ru>
2018-02-01 13:26:32 +0300

    Rename tests in integer-test;

--- js/tests/test-integer.js
+++ js/tests/test-integer.js
@@ -100,7 +100,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 1 to 1', function () {
+    it('of 1 sub 1', function () {
         var firstNumber = new Integer('1');
         var secondNumber = new Integer('1');
         var expected = new Integer('0');
@@ -110,7 +110,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 100 to 9', function () {
+    it('of 100 sub 9', function () {
         var firstNumber = new Integer('100');
         var secondNumber = new Integer('9');
         var expected = new Integer('91');
@@ -120,7 +120,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 21 to 30', function () {
+    it('of 21 sub 30', function () {
         var firstNumber = new Integer('21');
         var secondNumber = new Integer('30');
         var expected = new Integer('-9');
@@ -130,7 +130,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 1000 to 999', function () {
+    it('of 1000 sub 999', function () {
         var firstNumber = new Integer('1000');
         var secondNumber = new Integer('999');
         var expected = new Integer('1');
@@ -140,7 +140,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 1000000000000 to 999999999999', function () {
+    it('of 1000000000000 sub 999999999999', function () {
         var firstNumber = new Integer('1000000000000');
         var secondNumber = new Integer('999999999999');
         var expected = new Integer('1');
@@ -150,7 +150,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub -21 to -30', function () {
+    it('of -21 sub -30', function () {
         var firstNumber = new Integer('-21');
         var secondNumber = new Integer('-30');
         var expected = new Integer('9');
@@ -160,7 +160,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub -21 to 30', function () {
+    it('of -21 sub 30', function () {
         var firstNumber = new Integer('-21');
         var secondNumber = new Integer('30');
         var expected = new Integer('-51');
@@ -170,7 +170,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 21 to -30', function () {
+    it('of 21 sub -30', function () {
         var firstNumber = new Integer('21');
         var secondNumber = new Integer('-30');
         var expected = new Integer('51');
@@ -180,7 +180,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 100000000 to 1', function () {
+    it('of 100000000 sub 1', function () {
         var firstNumber = new Integer('100000000');
         var secondNumber = new Integer('1');
         var expected = new Integer('99999999');
@@ -190,7 +190,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub -645645776578 to 343255465768768798', function () {
+    it('of -645645776578 sub 343255465768768798', function () {
         var firstNumber = new Integer('-645645776578');
         var secondNumber = new Integer('343255465768768798');
         var expected = new Integer('-343256111414545376');
@@ -200,7 +200,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 354456436346 to 4364', function () {
+    it('of 354456436346 sub 4364', function () {
         var firstNumber = new Integer('354456436346');
         var secondNumber = new Integer('4364');
         var expected = new Integer('354456431982');
@@ -210,7 +210,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 965394583456 to 89534', function () {
+    it('of 965394583456 sub 89534', function () {
         var firstNumber = new Integer('965394583456');
         var secondNumber = new Integer('89534');
         var expected = new Integer('965394493922');
@@ -220,7 +220,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub 354456436346345745645684965394583456 to 4364565454645823653489534', function () {
+    it('of 354456436346345745645684965394583456 sub 4364565454645823653489534', function () {
         var firstNumber = new Integer('354456436346345745645684965394583456');
         var secondNumber = new Integer('4364565454645823653489534');
         var expected = new Integer('354456436341981180191039141741093922');
@@ -230,7 +230,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('sub -1 to -1000000000000', function () {
+    it('of -1 sub -1000000000000', function () {
         var firstNumber = new Integer('-1');
         var secondNumber = new Integer('-1000000000000');
         var expected = new Integer('999999999999');
@@ -240,7 +240,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 9 to 1', function () {
+    it('9 mul by 1', function () {
         var firstNumber = new Integer('9');
         var secondNumber = new Integer('10');
         var expected = new Integer('90');
@@ -250,7 +250,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 935345 to 0', function () {
+    it('935345 mul by 0', function () {
         var firstNumber = new Integer('935345');
         var secondNumber = new Integer('0');
         var expected = new Integer('0');
@@ -260,7 +260,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 0 to 0', function () {
+    it('0 mul by 0', function () {
         var firstNumber = new Integer('0');
         var secondNumber = new Integer('0');
         var expected = new Integer('0');
@@ -270,7 +270,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 4 to 4', function () {
+    it('4 mul by 4', function () {
         var firstNumber = new Integer('4');
         var secondNumber = new Integer('4');
         var expected = new Integer('16');
@@ -280,7 +280,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 32 to 32', function () {
+    it('32 mul by 32', function () {
         var firstNumber = new Integer('32');
         var secondNumber = new Integer('32');
         var expected = new Integer('1024');
@@ -290,7 +290,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 0 to 54364365345436', function () {
+    it('0 mul by 54364365345436', function () {
         var firstNumber = new Integer('0');
         var secondNumber = new Integer('54364365345436');
         var expected = new Integer('0');
@@ -300,7 +300,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 9187 to 189', function () {
+    it('9187 mul by 189', function () {
         var firstNumber = new Integer('9187');
         var secondNumber = new Integer('189');
         var expected = new Integer('1736343');
@@ -310,7 +310,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 9187 to -189', function () {
+    it('9187 mul by -189', function () {
         var firstNumber = new Integer('9187');
         var secondNumber = new Integer('-189');
         var expected = new Integer('-1736343');
@@ -320,7 +320,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul -9187 to 189', function () {
+    it('-9187 mul by 189', function () {
         var firstNumber = new Integer('-9187');
         var secondNumber = new Integer('189');
         var expected = new Integer('-1736343');
@@ -330,7 +330,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul -91874214143 to -32534563000', function () {
+    it('-91874214143 mul by -32534563000', function () {
         var firstNumber = new Integer('-91874214143');
         var secondNumber = new Integer('-32534563000');
         var expected = new Integer('2989087408110924509000');
@@ -340,7 +340,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mul 324144744621 to 1000000000000', function () {
+    it('324144744621 mul by 1000000000000', function () {
         var firstNumber = new Integer('324144744621');
         var secondNumber = new Integer('1000000000000');
         var expected = new Integer('324144744621000000000000');
@@ -350,7 +350,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 100000 to 10', function () {
+    it('100000 div by 10', function () {
         var firstNumber = new Integer('100000');
         var secondNumber = new Integer('10');
         var expected = new Integer('10000');
@@ -360,7 +360,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 81 to 27', function () {
+    it('81 div by 27', function () {
         var firstNumber = new Integer('81');
         var secondNumber = new Integer('3');
         var expected = new Integer('27');
@@ -370,7 +370,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 83 to 3', function () {
+    it('83 div by 3', function () {
         var firstNumber = new Integer('83');
         var secondNumber = new Integer('3');
         var expected = new Integer('27');
@@ -380,7 +380,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 0 to 27', function () {
+    it('0 div by 27', function () {
         var firstNumber = new Integer('0');
         var secondNumber = new Integer('27');
         var expected = new Integer('0');
@@ -390,7 +390,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 9 to 3', function () {
+    it('9 div by 3', function () {
         var firstNumber = new Integer('9');
         var secondNumber = new Integer('3');
         var expected = new Integer('3');
@@ -400,7 +400,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 10 to 3', function () {
+    it('10 div by 3', function () {
         var firstNumber = new Integer('10');
         var secondNumber = new Integer('3');
         var expected = new Integer('3');
@@ -410,7 +410,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 8 to 3', function () {
+    it('8 div by 3', function () {
         var firstNumber = new Integer('8');
         var secondNumber = new Integer('3');
         var expected = new Integer('2');
@@ -420,7 +420,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 0 to 3', function () {
+    it('0 div by 3', function () {
         var firstNumber = new Integer('0');
         var secondNumber = new Integer('3');
         var expected = new Integer('0');
@@ -430,7 +430,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 1 to 3', function () {
+    it('1 div by 3', function () {
         var firstNumber = new Integer('1');
         var secondNumber = new Integer('3');
         var expected = new Integer('0');
@@ -440,7 +440,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 8655288755589632247 to 4555534522452', function () {
+    it('8655288755589632247 div by 4555534522452', function () {
         var firstNumber = new Integer('8655288755589632247');
         var secondNumber = new Integer('4555534522452');
         var expected = new Integer('1899950');
@@ -451,7 +451,7 @@ describe('Tests for Integer', function () {
     })
 
 
-    it('div -8 to -3', function () {
+    it('-8 div by -3', function () {
         var firstNumber = new Integer('-8');
         var secondNumber = new Integer('-3');
         var expected = new Integer('2');
@@ -461,7 +461,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 12 to 2', function () {
+    it('12 div by 2', function () {
         var firstNumber = new Integer('12');
         var secondNumber = new Integer('2');
         var expected = new Integer('6');
@@ -472,7 +472,7 @@ describe('Tests for Integer', function () {
     })
 
 
-    it('div 0 to -3', function () {
+    it('0 div by -3', function () {
         var firstNumber = new Integer('0');
         var secondNumber = new Integer('-3');
         var expected = new Integer('0');
@@ -482,7 +482,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div -1 to 3', function () {
+    it('-1 div by 3', function () {
         var firstNumber = new Integer('-1');
         var secondNumber = new Integer('3');
         var expected = new Integer('0');
@@ -492,7 +492,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('div 8655288755589632247 to -4555534522452', function () {
+    it('8655288755589632247 div by -4555534522452', function () {
         var firstNumber = new Integer('8655288755589632247');
         var secondNumber = new Integer('-4555534522452');
         var expected = new Integer('-1899950');
@@ -502,7 +502,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mod 100 to 12', function () {
+    it('remainder of division 100 by 12', function () {
         var firstNumber = new Integer('100');
         var secondNumber = new Integer('12');
         var expected = new Integer('4');
@@ -512,7 +512,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mod 3379 to 52', function () {
+    it('remainder of division 3379 by 52', function () {
         var firstNumber = new Integer('3379');
         var secondNumber = new Integer('52');
         var expected = new Integer('51');
@@ -522,7 +522,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     })
 
-    it('mod 9 to 3', function () {
+    it('remainder of division 9 by 3', function () {
         var firstNumber = new Integer('9');
         var secondNumber = new Integer('3');
         var expected = new Integer('0');
@@ -532,7 +532,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('mod 3 to 2', function () {
+    it('remainder of division 3 by 2', function () {
         var firstNumber = new Integer('3');
         var secondNumber = new Integer('2');
         var expected = new Integer('1');
@@ -542,7 +542,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('mod 4 to 10', function () {
+    it('remainder of division 4 by 10', function () {
         var firstNumber = new Integer('4');
         var secondNumber = new Integer('10');
         var expected = new Integer('4');
@@ -553,7 +553,7 @@ describe('Tests for Integer', function () {
     });
 
 
-    it('pow 13 to 3', function () {
+    it('13 to the power of 3', function () {
         var firstNumber = new Integer('13');
         var secondNumber = new Integer('3');
         var expected = new Integer('2197');
@@ -563,7 +563,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 2 to 6', function () {
+    it('2 to the power of 6', function () {
         var firstNumber = new Integer('2');
         var secondNumber = new Integer('6');
         var expected = new Integer('64');
@@ -573,7 +573,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 15 to 2', function () {
+    it('15 to the power of 2', function () {
         var firstNumber = new Integer('15');
         var secondNumber = new Integer('2');
         var expected = new Integer('225');
@@ -583,7 +583,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 20 to 4', function () {
+    it('20 to the power of 4', function () {
         var firstNumber = new Integer('20');
         var secondNumber = new Integer('4');
         var expected = new Integer('160000');
@@ -593,7 +593,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 12 to 12', function () {
+    it('12 to the power of 12', function () {
         var firstNumber = new Integer('12');
         var secondNumber = new Integer('12');
         var expected = new Integer('8916100448256');
@@ -603,7 +603,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 12 to 11', function () {
+    it('12 to the power of 11', function () {
         var firstNumber = new Integer('12');
         var secondNumber = new Integer('11');
         var expected = new Integer('743008370688');
@@ -613,7 +613,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 12 to 5', function () {
+    it('12 to the power of 5', function () {
         var firstNumber = new Integer('12');
         var secondNumber = new Integer('5');
         var expected = new Integer('248832');
@@ -623,7 +623,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 2 to 10', function () {
+    it('2 to the power of 10', function () {
         var firstNumber = new Integer('2');
         var secondNumber = new Integer('10');
         var expected = new Integer('1024');
@@ -633,7 +633,7 @@ describe('Tests for Integer', function () {
         assert.deepEqual(actual, expected);
     });
 
-    it('pow 2 to 11', function () {
+    it('2 to the power of 11', function () {
         var firstNumber = new Integer('2');
         var secondNumber = new Integer('11');
         var expected = new Integer('2048');
