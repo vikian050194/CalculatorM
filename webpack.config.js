12c8e35120e7ccd439ae88e200c5aca4d57184d4
Kirill Vinogradov <vikian050194@gmail.com>
2020-10-06 23:40:26 +0300

    Misc changes

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const webpack = require("webpack");
 const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
+const buildFolderName = "public";
+
 process.env.NODE_ENV = "development";
 
 module.exports = {
